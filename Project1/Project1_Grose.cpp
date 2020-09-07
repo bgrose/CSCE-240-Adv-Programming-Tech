b0b6c521aaaa1e307d4fee19f9f64199f03203c1
bgrose <47512745+bgrose@users.noreply.github.com>
2020-09-07 17:30:37 -0400

    Adjustment

--- Project1/Project1_Grose.cpp
+++ Project1/Project1_Grose.cpp
@@ -1,8 +1,8 @@
 /*
- *   @author    Written By Bradley Grose
- *   @date      Completed On 8/31/2020 at 10:06PM
+ *   @author       Written By Bradley Grose
+ *   @date           Completed On 8/31/2020 at 10:06PM
  *   @summary   This Program will allow the user to give their first and last 
- *              name and then a command that will do an action this action will 
+ *                         name and then a command that will do an action this action will 
  *              either be Hello, Goodbye, or invalid
  *   @input     The input for the name can be any string, however the commands 
  *              must be "A" or "B" to run without invalid. The command is cap 
