b71f3088ff88a96561e387d0b6e520d77c3d162f
bgrose <47512745+bgrose@users.noreply.github.com>
2020-09-09 17:22:43 -0400

    Worked on Changes for Week 4

--- Week4/pointers.cpp
+++ Week4/pointers.cpp
@@ -10,8 +10,7 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int x(0);
     x = 100;
 
@@ -26,17 +25,44 @@ int main(int argc, char **argv)
     cout << "Location of xPtr is " << &xPtr << endl;
     cout << "value of xPtr is " << &xPtr << endl;
     cout << "value of what xPtr is pointing to is " << *xPtr << endl;
-    return 0;
+
+
+    //THIS IS NOT EFFICENT FOR MEMORY ALOCATION
 
     int arr[5] = {1, 1, 1, 1, 1};
 
     //Extend method
     int newArr[6] = {0};
 
-    for( int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         newArr[i] = arr[i];
     }
 
     newArr[5] = 10;
+
+
+    //DYNAMIC ARRAYS
+
+    int *arr;       //Pointer for Mem Location
+    //~ int arr1[0];
+    //~ cout << arr << endl;
+    //~ arr = arr1;     //Points to memory location
+
+    arr = new int[5];
+    for (int i = 0; i < 5; i++) {
+        arr[i] = 0;
+    }
+
+    int *temp
+    temp = arr;
+
+    arr = new int[6];
+    for (int i = 0; i < 5; i++) {
+        arr[i] = temp[i];
+    }
+    arr[5] = 0;
+
+    delete [] temp;
+
+    return 0;
 }
